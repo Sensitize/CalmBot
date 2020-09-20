344d012db7920b146dc6c45cbd1f950209b44840
Ryan Arora <arora.ryan@gmail.com>
2020-09-20 00:28:54 -0400

    fix: delete pingmiqhtie cmd

--- src/commands/pingmiqhtie.js
+++
@@ -1,8 +0,0 @@
-const pingmiqhtie = (client, message) => {
-    message.channel.send(`Ping <@438057670042320896>`).then((m) =>{
-        m.delete();
-        message.delete()
-    });
-}
-
-module.exports = pingmiqhtie;
