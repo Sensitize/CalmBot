18998be6eab04861d67be20af169762920f0b0e7
Joel <joelcedras@gmail.com>
2020-09-22 12:09:51 +0200

    add code to eval.js
    added the actual EVAL part.

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -1,1 +1,36 @@
-// Add file eval.js
+// Evaluates javascript code
+const eval = (client, message) => {
+if(message.author.id !== '438057670042320896') {        // Checks if author's ID equals Owner's ID.
+            return message.channel.send('Error! You are not my owner!');    // Return if false
+        }
+  // Borrowed some code from an-idiots-guide
+        try {
+            var code = message.args.join(" ");
+            var evaluated = eval(code);
+      
+            if (typeof evaluated !== "string")
+              evaluated = require("util").inspect(evaluated);
+            
+            const embed = new Discord.MessageEmbed()
+              .setColor(0x00A2E8)
+              .addField(":inbox_tray: Input: ", `\`\`\`${code}\`\`\``)
+              .addField(":outbox_tray: output: ", `\`\`\`js\n${clean(evaluated)}\n\`\`\``);
+            message.channel.send({embed})
+          } catch (err) {
+            const embed = new Discord.MessageEmbed()
+            .setColor(0x00A2E8)
+            .addField(":inbox_tray: Input: ", `\`\`\`${code}\`\`\``)
+            .addField(":outbox_tray: output: ", `\`\`\`${clean(err)}\`\`\``);
+          message.channel.send({embed})
+          }
+      
+    const clean = text => {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+}
+
+}
+
+module.exports = eval;
