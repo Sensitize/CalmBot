0e9316360bdab9a641e755d5385276de8e0c686e
Ryan Arora <arora.ryan@gmail.com>
2020-09-22 13:22:31 -0400

    feat: trivia command message added

--- src/commands/cat.js
+++ src/commands/cat.js
@@ -1,4 +1,4 @@
-const urls = require('../data/cat.json');
+const urls = require('../data/img/cat.json');
 
 const cat = (client, message) => {
   const img = urls[Math.floor(Math.random() * urls.length)];
