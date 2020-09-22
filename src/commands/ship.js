b57ae78f9854a2896a1f4ac53eab4f9ef318d4dd
Miqhtie <60279933+Miqhtiedev@users.noreply.github.com>
2020-09-22 10:12:06 -0400

    Ship Command

--- src/commands/ship.js
+++ src/commands/ship.js
@@ -1,11 +1,36 @@
-const seedrandom = require('seedrandom');
+const seedrandom = require("seedrandom");
 
 const ship = (client, message) => {
+  if (message.mentions.members.size) {
+    return message.channel.send(
+      "Don't mention people thats MEAN! Just say their name in text!"
+    );
+  }
+
+  if (message.args.length < 2) {
+    return message.channel.send(
+      "Please include 2 arguments Ex `c!ship (person 1) (person 2)`"
+    );
+  }
+
+  const args = message.args;
+
+  let person1 = args[0];
+  let person2 = args[1];
+
+  const oldstring = person1 + person2;
+  let str;
+
+  str = oldstring.replace(/[0-9]/g, '');
+
+
   const date = new Date().getDate().toString();
 
-  let filteredMessage = filter(message.content);
-  const rng = Math.floor(seedrandom(stringValue(filteredMessage) * 41798 + date).quick() * 100);
-  message.channel.send(`they are **${rng}%** compatible!`);
+  let filteredMessage = filter(str);
+  const rng = Math.floor(
+    seedrandom(stringValue(filteredMessage) * 41798 + date).quick() * 100
+  );
+  message.channel.send(`They are **${rng}%** compatible!`);
 };
 
 function stringValue(string) {
@@ -18,18 +43,17 @@ function stringValue(string) {
 }
 
 function alphabetPosition(text) {
-  var result = '';
+  var result = "";
   for (var i = 0; i < text.length; i++) {
     var code = text.toUpperCase().charCodeAt(i);
-    if (code > 64 && code < 91) result += code - 64 + ' ';
+    if (code > 64 && code < 91) result += code - 64 + " ";
   }
 
   return result.slice(0, result.length - 1);
 }
 
 function filter(text) {
-  let desired = text.replace(/[^a-zA-Z0-9]/gi, '');
-  console.log(desired);
+  let desired = text.replace(/[^a-zA-Z0-9]/gi, "");
   return desired;
 }
 
