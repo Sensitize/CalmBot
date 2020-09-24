2f7453bd3d9f0216156a4d1c428e65c73e514722
Joel <joelcedras@gmail.com>
2020-09-24 18:29:41 +0200

    add commenting > startup.js

--- src/commands/startup.js
+++ src/commands/startup.js
@@ -2,6 +2,7 @@ const startup = (client, message) => {
   if (!message.member.hasPermission("ADMINISTRATOR", { checkOwner: true })) {
     return message.channel.send("Error! You are not my owner!");
   } else {
+    // Changes bot from being shutdown to being up!
     client.settings.shutdown = false;
     message.reply("Bot is starting up!");
 
