20e8eb82d17c5f220c51948067d03e913270bc4f
Joel <joelcedras@gmail.com>
2020-09-24 18:27:42 +0200

    import code > startup.js

--- src/commands/startup.js
+++ src/commands/startup.js
@@ -1,1 +1,11 @@
-// Create startup.js
+const startup = (client, message) => {
+  if (!message.member.hasPermission("ADMINISTRATOR", { checkOwner: true })) {
+    return message.channel.send("Error! You are not my owner!");
+  } else {
+    client.settings.shutdown = false;
+    message.reply("Bot is starting up!");
+
+  }
+};
+
+module.exports = startup;
