775286a569189262f8dda9c275ce081d846a7c47
Joel <joelcedras@gmail.com>
2020-09-24 18:27:04 +0200

    import new code > shutdown.js

--- src/commands/shutdown.js
+++ src/commands/shutdown.js
@@ -2,7 +2,12 @@ const shutdown = (client, message) => {
   if (!message.member.hasPermission("ADMINISTRATOR", { checkOwner: true })) {
     return message.channel.send("Error! You are not my owner!");
   } else {
-    client.destroy();
+    if (!client.settings.shutdown) {
+      client.settings.shutdown = true;
+      message.reply("Bot is shutting down!");
+    } else {
+      message.reply("I'm ALREADY shut DOWN!!!!!!");
+    }
   }
 };
 
