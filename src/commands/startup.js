d5ec311b8609729f15544d7e7dd80504ba7195cb
Miqhtie <60279933+Miqhtiedev@users.noreply.github.com>
2020-09-24 12:44:23 -0400

    Change wording

--- src/commands/startup.js
+++ src/commands/startup.js
@@ -1,6 +1,6 @@
 const startup = (client, message) => {
   if (!message.member.hasPermission("ADMINISTRATOR", { checkOwner: true })) {
-    return message.channel.send("Error! You are not my owner!");
+    return message.channel.send("Error! You do not have permissions to start the bot!");
   } else {
     // Changes bot from being shutdown to being up!
     client.settings.shutdown = false;
