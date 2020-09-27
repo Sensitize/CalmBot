7d2b13854375416f20f16cdcd1a5e19544fc345d
Miqhtie <60279933+Miqhtiedev@users.noreply.github.com>
2020-09-27 16:08:32 -0400

    remove cheems.js

--- src/commands/cheems.js
+++
@@ -1,8 +0,0 @@
-const urls = require('../data/img/cheems.json');
-
-const cheems = (client, message) => {
-  const img = urls[Math.floor(Math.random() * urls.length)];
-  message.channel.send(img);
-};
-
-module.exports = cheems;
