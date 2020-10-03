fbd385035bc2839cf716f321f8bdab32c6aaa03a
Ryan Arora <arora.ryan@gmail.com>
2020-10-03 12:33:00 -0400

    fix: commandPrefix in status

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,4 @@
 module.exports = async function ready(client) {
   console.log(`${client.user.tag} is serving ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`);
-  client.user.setActivity(`prefix "${client.settings.prefix}"`, { type: 'LISTENING' });
+  client.user.setActivity(`prefix "${client.commandPrefix}"`, { type: 'LISTENING' });
 };
