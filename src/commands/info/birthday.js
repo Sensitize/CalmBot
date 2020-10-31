ff30992fc9f1ef8ab7d8ec74c521349276414ce3
Joel Cedras <joelcedras@gmail.com>
2020-10-31 17:42:21 +0200

    [+] LAST SECOND CHANGES

--- src/commands/info/birthday.js
+++ src/commands/info/birthday.js
@@ -1,6 +1,6 @@
 const { Command } = require('discord.js-commando');
 
-module.exports = class RolesCommand extends Command {
+module.exports = class BirthdayCommand extends Command {
   constructor(client) {
     super(client, {
       name: 'birthday',
