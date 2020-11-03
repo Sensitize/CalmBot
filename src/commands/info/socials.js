bdfa70ca04756f5ff2aa1ceaca3e9eebd850d93c
Joel <joelcedras@gmail.com>
2020-11-03 20:28:23 +0200

    fix '

--- src/commands/info/socials.js
+++ src/commands/info/socials.js
@@ -18,7 +18,7 @@ module.exports = class SocialsCommand extends Command {
       'Plancke: <https://plancke.io/hypixel/guild/name/calm>\n' +
       'Forums Thread: <https://hypixel.net/threads/3013892>\n' +
       'YouTube: <https://www.youtube.com/channel/UC5NBW0EG7fFRKztTL4U96AQ>\n\n' +
-      '**Guild Master's Media**\n' +
+      '**Guild Master\'s Media**\n' +
       'Twitter: <https://twitter.com/ignhopez>\n' +
       'Plancke: <https://plancke.io/hypixel/player/stats/hopez>\n' +
       'Forums: <https://hypixel.net/members/1689275>\n' +
