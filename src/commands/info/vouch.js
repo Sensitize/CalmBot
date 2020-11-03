0dd293b74a17ed42cb7f30da7554d665a574ae34
Joel <joelcedras@gmail.com>
2020-11-03 20:22:45 +0200

    [+] Updated Requirements for vouch

--- src/commands/info/vouch.js
+++ src/commands/info/vouch.js
@@ -22,7 +22,7 @@ module.exports = class VouchCommand extends Command {
     const send =
     '**Vouch System**\n' +
     'To be eligable to vouch a member in Calm, you must be Trusted+.\n' +
-    'To be vouched into Calm, you must be no lower than Hypixel level 65.\n\n' +
+    'To be vouched into Calm, you must be no lower than Hypixel level 75.\n\n' +
     'Each rank below gets the number of vouches listed PER MONTH.\n\n' +
     'Trusted: 3\nLoyal: 4\nOG: 5\nGuild Staff: 6\n\n' +
     `*If you have any questions, please do \`t!open\` in ${commandsChannel} to open a ticket and talk to staff*`;
