fe10f4c6ab8dfc969007f1d925d7ff4bd09457b9
Joel Cedras <joelcedras@gmail.com>
2020-11-10 18:05:35 +0200

    [+] Poll Command Functionality

--- src/commands/util/poll.js
+++ src/commands/util/poll.js
@@ -1,4 +1,5 @@
 const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = class PollCommand extends Command {
   constructor(client) {
@@ -9,6 +10,13 @@ module.exports = class PollCommand extends Command {
       description: 'Creates a poll to be voted on',
       examples: [`${client.commandPrefix}poll Is Miqhtie Cool?`],
       guildOnly: true,
+      args: [
+        {
+          key: 'poll',
+          prompt: 'please reply with your poll!',
+          type: 'string',
+        },
+      ],
     });
   }
 
@@ -16,7 +24,35 @@ module.exports = class PollCommand extends Command {
     return message.member.hasPermission('ADMINISTRATOR', { checkOwner: false });
   }
 
-  async run(message) {
+  async run(message, { poll }) {
+    let firstReaction, secondReaction;
+    if (message.guild.id === '501501905508237312'){
+        firstReaction = '615239771723137026';     //  https://cdn.discordapp.com/emojis/615239771723137026.png?v=1
+        secondReaction = '615239802127777817';    // https://cdn.discordapp.com/emojis/615239802127777817.png?v=1
+      } else {
+        firstReaction = '✅';
+        secondReaction = '❎';
+      }
 
+      let pollEmbed = new MessageEmbed()
+      .setFooter(`${message.member.displayName}`, message.author.displayAvatarURL())
+      .setColor('#007FFF')
+      .setTitle("New Poll:")
+      .setDescription(poll)
+      .setTimestamp();
+
+
+      try{
+        message.channel.send({embed: pollEmbed}).then(sentEmbed => {
+        message.delete();
+        sentEmbed.react(firstReaction);
+        sentEmbed.react(secondReaction);
+      });
+    } catch{
+      message.channel.send("Uh oh! Something went wrong while attempting to create a poll!");
+      return;
+    }
+    
+      
   }
 };
